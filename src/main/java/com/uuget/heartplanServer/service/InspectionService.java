57112deed65b32d451595d3589a0fb4d3e7affa5
bk201c <bk201c@outlook.com>
2022-07-15 17:38:42 +0800

    add

---
+++ src/main/java/com/uuget/heartplanServer/service/InspectionService.java
@@ -0,0 +1,8 @@
+package com.uuget.heartplanServer.service;
+
+import com.uuget.heartplanServer.model.entity.Inspection;
+
+public interface InspectionService {
+  Inspection getInspectionById(Integer id);
+
+}
